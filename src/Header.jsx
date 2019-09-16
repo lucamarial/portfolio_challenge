b9539452403045d6b545c42e00272e5825e3e619
luca <l.lobacher@web.de>
2019-09-16 22:21:54 +0200

    Adds react components Hello, Header and Footer
    Adds semantic ui and css stylesheet.

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <nav className='ui fixed inverted menu'>
+      <div className="ui container">
+        <h3 className="header item">
+          <h1> My Portfolio</h1>
+        </h3>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
